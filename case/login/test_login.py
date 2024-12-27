d0130fff4b4746c149f103d4cde48d2b70593bd6
axie <1282944026@qq.com>
2024-12-27 17:03:28 +0800

    创建项目

---
+++ case/login/test_login.py
@@ -0,0 +1,30 @@
+
+# coding=utf-8
+import pytest
+import allure
+from utils.handle_yaml_tips.analyse_yaml_data import AnalyseYamlData
+from utils.execute_tips.base_requests import BaseRequests
+
+
+ayd = AnalyseYamlData('login', 'login')
+allure_params = ayd.analyse_yaml_data()[0]
+case_title = ayd.analyse_yaml_data()[1]
+case_list = ayd.analyse_yaml_data()[2]
+
+
+@allure.epic(allure_params['allureEpic'])
+@allure.feature(allure_params['allureFeature'])
+@allure.story(allure_params['allureStory'])
+@pytest.mark.parametrize("case", case_list, ids=case_title)
+def test_login(case):
+    br = BaseRequests(case)
+    result = br.base_requests()
+    if result is None:
+        pytest.skip("")
+    assert result
+
+
+if __name__ == '__main__':
+    pytest.main(["-s","-q","test_login.py"])       
+        
+        
---
+++ test.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# @Time    : 2024/12/24 15:50
+# @Author  : 谢阿谢
+# @File    : test.py
+# @Project : auto-api-v1
+import sys
+print(sys.path)
+
